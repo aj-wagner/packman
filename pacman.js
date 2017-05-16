d16634f9891e0f7024146f488df3d5b61965f64a
AJ Wagner <ajwagner@northwesternmutual.com>
2017-05-16 13:44:59 -0500

    latest

--- pacman.js
+++ pacman.js
@@ -52,6 +52,48 @@
 
 		return boardHTMLstr;
 	}
-		$(document).ready(function(){
-			$('#board').html(buildBoardStr(board));
-		});
+	
+	$(document).ready(function(){
+		$('#board').html(buildBoardStr(board));
+	});
+
+	function collisionDetection(pos){
+        //check pac man coordinates
+        var coor = $('#pacman img').position()
+        if(pos=="left" && coor.left<306){
+            return false
+        }else if(pos=="right" && coor.left>898){
+            return false
+        }else if(pos=="up" && coor.top<78){
+            return false
+        }else if(pos=="down" && coor.top > 360){
+            return false
+        }else{
+            return true;
+        }                
+    }
+
+	document.onkeydown = function(e){
+		//35 LR
+		//55 U&D
+		console.log($('#pacman img').position().left);
+		//left
+		if(e.keyCode == 37 && collisionDetection('left')){
+			$('#pacman img').animate({"left": "-=350%" }, "slow");
+		}
+		//right
+		else if(e.keyCode == 39 && collisionDetection('right')){
+			$('#pacman img').animate({"left": "+=350%" }, "slow");
+		}
+		//up
+		else if(e.keyCode == 38 && collisionDetection('up')){
+			$('#pacman img').animate({"top": "-=12.5%" }, "slow");
+		}
+		//down
+		else if(e.keyCode == 40 && collisionDetection('down')){
+			$('#pacman img').animate({"top": "+=12.5%" }, "slow");
+		}
+	}
+
+
+	
